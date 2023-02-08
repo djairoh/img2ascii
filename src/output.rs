5f01bfa485e682e798d8c8ef3840823779fee3b5
djairoh <ljxjah@mykolab.com>
2023-02-08 18:41:43 +0100

    small tweak to coloured output

--- src/output.rs
+++ src/output.rs
@@ -3,18 +3,19 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::process::exit;
 use log::error;
+use termion::{color, style};
 use crate::model_rgb_ascii::Ascii;
 
 pub fn print_terminal(art: Vec<Vec<Ascii>>, in_colour: bool) {
     for line in art {
         for ascii in line {
             if in_colour {
-                print!("{}{}", termion::color::Fg(termion::color::Rgb(ascii.rgb.r, ascii.rgb.g, ascii.rgb.b)), ascii.char);
+                print!("{}{}", color::Fg(color::Rgb(ascii.rgb.r, ascii.rgb.g, ascii.rgb.b)), ascii.char);
             } else {
                 print!("{}", ascii.char);
             }
         }
-        println!("{}", termion::style::Reset);
+        println!("{}", style::Reset);
     }
 }
 
